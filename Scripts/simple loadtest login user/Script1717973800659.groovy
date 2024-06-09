9d30300319560a5a9094d974b2c57ea42b36573e
ZalKurniawan <sinsgreed60@gmail.com>
2024-06-10 06:18:47 +0700

    add simple loadtest

--- Scripts/simple loadtest login user/Script1717973800659.groovy
+++ Scripts/simple loadtest login user/Script1717973800659.groovy
@@ -16,22 +16,18 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import groovy.json.JsonSlurper as JsonSlurper
 import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
+import java.util.concurrent.Executors as Executors
+import java.util.concurrent.TimeUnit as TimeUnit
 
-'Send request'
-def response = WS.sendRequest(findTestObject('Object Repository/login_user'))
+int numRequests = 10
 
-'Verify status code'
-WS.verifyResponseStatusCode(response, 200)
+// Mengirim permintaan secara berurutan
+for (int i = 0; i < numRequests; i++) {
+    def response = WS.sendRequest(findTestObject('Object Repository/login_user'))
 
-'parse JSON response'
-def jsonResponse = new JsonSlurper().parseText(response.getResponseText())
+    KeywordUtil.logInfo('Response: ' + response.getResponseText())
 
-'Verify token not null'
-if (jsonResponse.token == null) {
-    throw new AssertionError('Token tidak ada dalam respons')
-} else {
-    'print token'
-    KeywordUtil.logInfo('Token: ' + jsonResponse.token)
 }
+
+KeywordUtil.logInfo('Load test selesai.')
